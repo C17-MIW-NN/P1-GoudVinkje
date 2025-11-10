eab2762b524a087f46e2a5b5a16bcce019eccd9d
Simon van der Kooij <famvanderkooij@gmail.com>
2025-11-10 14:35:03 +0100

    homepageController lite configuration security

---
+++ src/main/java/nl/miwnn/ch17/pixeldae/goudvinkje/config/GoudVinkjeSecurityConfig.java
@@ -0,0 +1,33 @@
+package nl.miwnn.ch17.pixeldae.goudvinkje.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Simon van der Kooij
+ * configure the security for the GoudVinkje application
+ */
+@Configuration
+@EnableWebSecurity
+public class GoudVinkjeSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/css/**", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout((logout) -> logout.logoutSuccessUrl("/"));
+
+        return httpSecurity.build();
+    }
+
+}
