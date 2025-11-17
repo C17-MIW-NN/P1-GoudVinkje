c8b6487fd1c5484ed54ca92003629b45b167cb3e
Annelies Hofman <annelies.hofman.art@gmail.com>
2025-11-17 11:03:16 +0100

    Checkbox cross through javascript works for both ingredients and steps for recipeDetail

--- src/main/resources/static/js/recipeDetailScript.js
+++ src/main/resources/static/js/recipeDetailScript.js
@@ -1,20 +1,8 @@
-document.querySelectorAll('.step-checkbox').forEach(checkbox => {
+document.querySelectorAll('.cross-checkbox').forEach(checkbox => {
     checkbox.addEventListener('change', function() {
-        const stepId = this.dataset.stepId;
-        const textElement = document.getElementById('instruction-' + stepId);
-
-        if (this.checked) {
-            textElement.classList.add('checked')
-        } else {
-            textElement.classList.remove('checked');
-        }
-    });
-});
-
-document.querySelectorAll('.ingredient-checkbox').forEach(checkbox => {
-    checkbox.addEventListener('change', function() {
-        const ingredientId = this.dataset.ingredientId;
-        const textElement = document.getElementById('description-' + ingredientId);
+        const id = this.dataset.id;
+        const type = this.dataset.type;
+        const textElement = document.getElementById(`${type}${id}`);
 
         if (this.checked) {
             textElement.classList.add('checked')
