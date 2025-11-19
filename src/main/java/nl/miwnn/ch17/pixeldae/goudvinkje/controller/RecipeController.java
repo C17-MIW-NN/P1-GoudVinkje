31b7695b4e09551ec2b89667d31ba25bd01f4d4a
Simon van der Kooij <famvanderkooij@gmail.com>
2025-11-19 18:37:46 +0100

    troubleshooting: repairing disappearing images, deleted ingredients, 2 portions ipv 0

--- src/main/java/nl/miwnn/ch17/pixeldae/goudvinkje/controller/RecipeController.java
+++ src/main/java/nl/miwnn/ch17/pixeldae/goudvinkje/controller/RecipeController.java
@@ -69,6 +69,7 @@ public class RecipeController {
         recipe.getRecipeHasIngredients().add(new RecipeHasIngredient(new Ingredient()));
         recipe.setAuthor(goudVinkjeUserService.getLoggedInUser());
         recipe.setPubliclyVisible(true);
+        recipe.setNrOfPortions(2);
 
         return showRecipeForm(datamodel, recipe);
     }
@@ -97,11 +98,12 @@ public class RecipeController {
         preventDuplicateIngredients(recipe);
 
         if (!result.hasErrors()) {
-            ifRecipeExistsRemoveAllIngredients(recipe);
-
+            // If it's someone else's recipe, make a copy.
             GoudVinkjeUser loggedInUser = goudVinkjeUserService.getLoggedInUser();
             if (!recipe.getAuthor().getUsername().equals(loggedInUser.getUsername())) {
                 recipe = recipe.newCopiedRecipe(loggedInUser);
+            } else {
+                ifRecipeExistsRemoveAllIngredients(recipe);
             }
 
             recipe.setAuthor(loggedInUser);
@@ -147,6 +149,6 @@ public class RecipeController {
     @GetMapping("/verwijderen/{recipeID}")
     public String deleteRecipe(@PathVariable("recipeID") Long recipeID) {
         recipeRepository.deleteById(recipeID);
-        return "redirect:/recept/";
+        return "redirect:/recept/overzicht";
     }
 }
